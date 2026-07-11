0778c23cfbf21b8f187d7f8d2d5f546c2a9cc09c
Hiroto0202 <hnd.hrt.02.02@gmail.com>
2026-07-11 13:56:28 +0900

    ステータス、山札のテキストが増減する処理を追加

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,18 @@ public class GameManager : MonoBehaviour
     [SerializeField] TextMesh hpTextP1;
     [SerializeField] TextMesh hpTextP2;
 
+    [SerializeField] TextMesh mpTextP1;
+    [SerializeField] TextMesh mpTextP2;
+
+    [SerializeField] TextMesh atkTextP1;
+    [SerializeField] TextMesh atkTextP2;
+
+    [SerializeField] TextMesh defTextP1;
+    [SerializeField] TextMesh defTextP2;
+
+    [SerializeField] TextMesh deckTextP1;
+    [SerializeField] TextMesh deckTextP2;
+
     int commandCount = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -33,36 +45,74 @@ public class GameManager : MonoBehaviour
         switch (com)
         {
             case 0:
+                // MP1の攻撃
                 P1.HandleStateTransition();
+                SetMPText(mpTextP1, 9);
                 break;
             case 1: 
                 P1.HandleStateTransition();
-                SetHPText(hpTextP2, 950);
+                SetHPText(hpTextP2, 1880);
                 break;
             case 2:
+                SetMPText(mpTextP1, 10);
+
+                // MP2のバフを使ってMP1の攻撃
                 P2.HandleStateTransition();
+                SetMPText(mpTextP2, 5);
+                SetAttackText(atkTextP2, 240);
                 break;
             case 3:
                 P2.HandleStateTransition();
-                SetHPText(hpTextP1, 900);
+                SetHPText(hpTextP1, 800);
                 break;
             case 4:
+                SetMPText(mpTextP2, 8);
+
+                // ドローしてMP2の防御バフカードを使う
                 P1.ChangeLastCard(draw1);
+                SetDeckText(deckTextP1, 34);
                 break;
             case 5:
                 P1.HandleStateTransition();
+                SetMPText(mpTextP1, 8);
+                SetDefenseText(defTextP1, 90);
+                Debug.Log("P1の防御バフカードを使用");
                 break;
             case 6:
-                P1.HandleStateTransition();
+                SetMPText(mpTextP1, 10);
+
+                // ドローしてMP2の攻撃バフカードを使う
+                P2.ChangeLastCard(draw2);
+                SetDeckText(deckTextP2, 34);
                 break;
             case 7:
-                P2.ChangeLastCard(draw1);
+                SetMPText(mpTextP2, 5);
+                SetAttackText(atkTextP2, 480);
+                P2.HandleStateTransition();
                 break;
             case 8:
-                P2.HandleStateTransition();
+                SetMPText(mpTextP2, 8);
+
+                // 先攻がドローして必殺技
+                P1.ChangeLastCard(draw1);
+                SetDeckText(deckTextP1, 33);
                 break;
             case 9:
+                SetMPText(mpTextP1, 2);
+                P1.HandleStateTransition();
+                SetHPText(hpTextP2, 1600);
+                break;
+            case 10:
+                SetMPText(mpTextP1, 10);
+
+                // 後攻がドローして必殺技
+                P2.ChangeLastCard(draw2);
+                SetDeckText(deckTextP2, 33);
+                break;
+            case 11:
+                SetMPText(mpTextP2, 3);
                 P2.HandleStateTransition();
+                SetHPText(hpTextP1, 0);
                 break;
         }
     }
@@ -70,5 +120,25 @@ public class GameManager : MonoBehaviour
     void SetHPText(TextMesh hpText, int newHP)
     {
         hpText.text = "HP: " + newHP.ToString();
+    }
+
+    void SetMPText(TextMesh mpText, int newMP)
+    {
+        mpText.text = "MP: " + newMP.ToString();
+    }
+
+    void SetAttackText(TextMesh atkText, int newATK)
+    {
+        atkText.text = "ATK: " + newATK.ToString();
+    }
+
+    void SetDefenseText(TextMesh defText, int newDEF)
+    {
+       defText.text = "DEF: " + newDEF.ToString();
+    }
+
+    void SetDeckText(TextMesh deckText, int newDeck)
+    {
+        deckText.text = newDeck.ToString();
     }
 }
